9bd9dc014289fa6b05aa145c7f554e7201c42019
youssef ahmed <youssefahmed052@gmail.com>
2023-05-06 11:27:11 +0300

    sortings done

--- sortings/sortings.cpp
+++ sortings/sortings.cpp
@@ -1,11 +1,10 @@
+//
+// Created by youssef zakaria on 3/23/2023.
+//
 #include "sortings.h"
-#include <bits/stdc++.h>
-using namespace std;
-
-
 template<typename T>
 void sortings::insertionSort(T arr[], int n) {
-    int i, key, j;
+    T i, key, j;
     for (int i = 0; i < n; ++i) {
         key = arr[i];
         j = i - 1;
@@ -16,6 +15,7 @@ void sortings::insertionSort(T arr[], int n) {
         arr[j + 1] = key;
     }
 }
+
 
 template<typename T>
 void sortings::selectionSort(T arr[], int n) {
@@ -41,3 +41,113 @@ void sortings::bubbleSort(T arr[], int n) {
         }
     }
 }
+
+template<typename T>
+void sortings::merge(T arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    T left[n1];
+    T right[n2];
+
+    for (int i = 0; i < n1; ++i) {
+        left[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; ++j) {
+        right[j] = arr[m + 1 + j];
+    }
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (left[i] < right[j]) {
+            arr[k] = left[i];
+            i++;
+        } else {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+template<typename T>
+void sortings::mergeSort(T arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+template<typename T>
+int sortings::partition(T arr[], int start, int end) {
+    int pivot = arr[end];
+    int pivotIndex = start;
+    for (int i = start; i < end; ++i) {
+        if (arr[i] <= pivot) {
+            swap(arr[i], arr[pivotIndex]);
+            pivotIndex++;
+        }
+    }
+    swap(arr[end], arr[pivotIndex]);
+    return pivotIndex;
+}
+
+template<typename T>
+void sortings::quickSort(T arr[], int start, int end) {
+    if (start < end) {
+        int pivotIndex = partition(arr, start, end);
+        quickSort(arr, start, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, end);
+    }
+}
+
+template<typename T>
+void sortings::shellSort(T arr[], int n) {
+    int gap = n / 2, temp, i, j;
+    for (gap; gap >= 1; gap /= 2) {
+        for (i = gap; i < n; ++i) {
+            temp = arr[i];
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+
+template<class T>
+void sortings::countSort(T arr[], int size) {
+    T mx = 0;
+    for (int i = 0; i < size; ++i) {
+        mx = max(mx, arr[i]);
+    }
+    vector<T> freq(mx + 1);
+    for (int i = 0; i < size; ++i) {
+        freq[arr[i]]++;
+    }
+    for (int i = 1; i <= mx; ++i) {
+        freq[i] += freq[i - 1];
+    }
+    T sortedArr[size];
+    for (int i = size - 1; i >= 0; --i) {
+        sortedArr[freq[arr[i]] - 1] = arr[i];
+        freq[arr[i]]--;
+    }
+    for (int i = 0; i < size; ++i) {
+        arr[i] = sortedArr[i];
+    }
+}
+
--- sortings/sortings.h
+++ sortings/sortings.h
@@ -1,41 +1,44 @@
+//
+// Created by youssef zakaria on 3/23/2023.
+//
+#include "bits/stdc++.h"
+using namespace std;
 #ifndef DATASTRUCTURE_ASSIGNMENT_1_SORTINGS_H
 #define DATASTRUCTURE_ASSIGNMENT_1_SORTINGS_H
 
-#include <iostream>
-#include <algorithm>
 
 class sortings {
 
 public:
-    template<typename T>
-    void insertionSort(T arr[], int n) ;
+template<typename T>
+void insertionSort(T arr[], int n) ;
 
-    template<typename T>
-    void selectionSort(T arr[], int n);
+template<typename T>
+void selectionSort(T arr[], int n);
 
-    template<typename T>
-    void bubbleSort(T arr[], int n);
+template<typename T>
+void bubbleSort(T arr[], int n);
 
-    template<typename T>
-    void merge(T arr[], int l, int m, int r) ;
+template<typename T>
+void merge(T arr[], int l, int m, int r) ;
 
-    template<typename T>
-    void mergeSort(T arr[], int l, int r);
+template<typename T>
+void mergeSort(T arr[], int l, int r);
 
-    template<typename T>
-    int partition(T arr[], int start, int end) ;
+template<typename T>
+int partition(T arr[], int start, int end) ;
 
-    template<typename T>
-    void quickSort(T arr[], int start, int end) ;
+template<typename T>
+void quickSort(T arr[], int start, int end) ;
 
-    template<typename T>
-    void shellSort(T arr[], int n) ;
+template<typename T>
+void shellSort(T arr[], int n) ;
 
 
-    template<class T>
-    void countSort(T arr[], int size);
+template<class T>
+void countSort(T arr[], int size);
 
 };
 
-#endif 
 
+#endif //DATASTRUCTURE_ASSIGNMENT_1_SORTINGS_H
